f2159603c02317e831ead5649a287f75bf6b2931
Bhargav Gohil <bhargavgohil25@gmail.com>
2020-11-28 21:20:27 +0530

    solved

---
+++ 1454D.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+#define rep(i,a,b) for(int i=a; i<b; i++)
+#define repe(i,a,b) for(int i=a; i<=b; i++)
+#define ll long long
+#define ui unsigned int
+#define pb push_back
+#define deb(x) cout << #x << '=' << x << endl
+#define deb2(x,y) cout << #x << '=' << x << << #y << '=' << y << endl
+#define all(x) x.begin(), x.end()
+#define endl "\n"
+#define ff first
+#define ss second
+#define lb lower_bound
+#define ub upper_bound
+#define INF (int)1e9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+#define hcf(x,y)     __gcd(x,y)
+#define lcm(x,y)     (x*y)/(__gcd(x,y))
+#define CountOne(x)  __builtin_popcount(x)        
+#define parity(x)    __builtin_parity(x)           //Funtion return (true) if number of set bits is odd(odd parity) else false..
+#define LeadZero(x)  __builtin_clz(x)             //Counts leading zero in binary representation of x ....   
+#define TrailZero(x) __builtin_ctz(x)
+
+#define fastIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef map<int, int> mii;
+typedef unordered_map<int, int> umap_ii;
+typedef unordered_map<int, int> umap_ii;
+typedef unordered_map<string, int> umap_si;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+//alt + ctrl + N
+
+//===============================================================================//
+ll power(ll x, ll y) {
+ll v = 1; while (y > 0) { if (y & 1)v = v * x; y = y >> 1; x = x * x;} return v;
+}
+
+bool isPowerOfTwo(ll x){
+    return x && (!(x & (x-1)));     //check if the number is power of two or not
+}
+
+//===============================================================================//
+
+int main(){
+    fastIO;
+srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+
+    ll test = 1;
+    cin >> test;
+    while(test--){
+        long long n;
+        cin>>n;
+        vector<pair<ll,ll>>v;
+        for(ll i=2;i*i<=n;i++){
+            int cnt=0;
+            while(n%i==0){
+                cnt++;
+                n/=i;
+            }
+            if(cnt>0)
+                v.push_back({cnt,i});
+        }
+        if(n>1)
+            v.push_back({1,n});
+        sort(v.begin(),v.end());
+        reverse(v.begin(),v.end());
+        vector<long long>ans;
+        // cout<<"in"<<"\n";
+        // for(auto i : v){
+        //     cout<<i.first<<" "<<i.second<<"\n";
+        // }
+        for(int i=0;i<v[0].first;i++)
+            ans.push_back(v[0].second);
+        for(int i=1;i<(int)v.size();i++){
+            for(int j=0;j<v[i].first;j++)
+                ans[v[0].first-1]*=v[i].second;
+        }
+        cout<<v[0].first<<"\n";
+        for(auto i : ans)
+            cout<<i<<" ";
+        cout<<"\n";
+
+    }
+return 0;
+}
